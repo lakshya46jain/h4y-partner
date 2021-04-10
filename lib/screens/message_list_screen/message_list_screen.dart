5d3ce6393ac2cff683bf165fa78e31ee92bfd5de
Lakshya Jain <ljain04062005@outlook.com>
2021-04-10 23:55:02 +0530

    Created Basic Message List UI

---
+++ lib/screens/message_list_screen/message_list_screen.dart
@@ -0,0 +1,31 @@
+// Flutter Imports
+import 'package:flutter/material.dart';
+// Dependency Imports
+// File Imports
+import 'package:h4y_partner/screens/message_list_screen/body.dart';
+import 'package:h4y_partner/screens/message_list_screen/app_bar.dart';
+
+class MessageListScreen extends StatefulWidget {
+  @override
+  _MessageListScreenState createState() => _MessageListScreenState();
+}
+
+class _MessageListScreenState extends State<MessageListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(
+            MediaQuery.of(context).size.height / (1792 / 100),
+          ),
+          child: MessageListAppBar(),
+        ),
+        body: Body(),
+      ),
+    );
+  }
+}
