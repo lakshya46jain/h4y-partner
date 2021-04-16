c7e7ce7209016816efb1b6ca05165bcfd373d9cd
Lakshya Jain <ljain04062005@outlook.com>
2021-04-16 23:17:34 +0530

    Created Settings Screen Body UI

--- lib/screens/settings_screen/body.dart
+++ lib/screens/settings_screen/body.dart
@@ -1,11 +1,45 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
 // Dependency Imports
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 // File Imports
+import 'package:h4y_partner/constants/expanded_button.dart';
+import 'package:h4y_partner/screens/settings_screen/stream_builder.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ProfileCardStreamBuilder(),
+          ExpandedButton(
+            icon: FluentIcons.info_24_regular,
+            text: "About Us",
+            onPressed: () {},
+          ),
+          ExpandedButton(
+            icon: FluentIcons.person_feedback_24_regular,
+            text: "Feedback",
+            onPressed: () {},
+          ),
+          ExpandedButton(
+            icon: FluentIcons.star_24_regular,
+            text: "Rate Us",
+            onPressed: () {},
+          ),
+          ExpandedButton(
+            icon: FluentIcons.share_24_regular,
+            text: "Share Help4You",
+            onPressed: () {},
+          ),
+          ExpandedButton(
+            icon: FluentIcons.sign_out_24_regular,
+            text: "Sign Out",
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
