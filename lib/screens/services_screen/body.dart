1d4386d23d11f7884c424971dfa298600d4d055d
Lakshya Jain <ljain04062005@outlook.com>
2021-04-17 23:40:17 +0530

    Created Body For Services Screen

--- lib/screens/services_screen/body.dart
+++ lib/screens/services_screen/body.dart
@@ -1,11 +1,44 @@
 // Flutter Imports
 import 'package:flutter/material.dart';
 // Dependency Imports
+import 'package:provider/provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 // File Imports
+import 'package:h4y_partner/models/user_model.dart';
+import 'package:h4y_partner/constants/loading.dart';
+import 'package:h4y_partner/screens/services_screen/service_tile.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Get User
+    final user = Provider.of<Help4YouUser>(context);
+
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("H4Y Users Database")
+          .doc(user.uid)
+          .collection("Services")
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot documentSnapshot = snapshot.data.docs[index];
+              return ServiceTile(
+                documentId: documentSnapshot.id,
+                serviceTitle: documentSnapshot["Service Title"],
+                serviceDescription: documentSnapshot["Service Description"],
+                servicePrice: documentSnapshot["Service Price"],
+                visibility: documentSnapshot["Visibility"],
+              );
+            },
+          );
+        } else {
+          return PouringHourGlassPageLoad();
+        }
+      },
+    );
   }
 }
