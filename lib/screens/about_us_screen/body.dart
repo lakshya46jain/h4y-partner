538a572cad105cf1325f5582551438af66f2fba6
Lakshya Jain <ljain04062005@outlook.com>
2021-04-17 18:01:14 +0530

    Given Functionality To Privacy Policy Button (About Us Page)

--- lib/screens/about_us_screen/body.dart
+++ lib/screens/about_us_screen/body.dart
@@ -50,7 +50,16 @@ class Body extends StatelessWidget {
           ExpandedButton(
             text: "Privacy Policy",
             icon: FluentIcons.lock_shield_24_regular,
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return PolicyDialog(
+                    mdFileName: 'privacy_policy.md',
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
