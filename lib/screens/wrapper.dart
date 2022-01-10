048b18044930f3da56222008431f46c38b79ea53
lakshyaj46 <lakshyaj465@gmail.com>
2022-01-10 22:07:55 +0530

    Implemented Internet Connectivity Checker on App Start

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -4,12 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:animations/animations.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 // File Imports
 import 'package:h4y_partner/models/user_model.dart';
 import 'package:h4y_partner/screens/bottom_nav_bar.dart';
+import 'package:h4y_partner/screens/server_error_screen.dart';
 import 'package:h4y_partner/screens/onboarding_screen/onboarding_screen.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  // Check Internet Connectivity
+  bool hasConnection;
+  checkInternetConnectivity() async {
+    hasConnection = await InternetConnectionChecker().hasConnection;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    checkInternetConnectivity();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     // Get User
@@ -30,7 +50,13 @@ class Wrapper extends StatelessWidget {
           child: child,
         );
       },
-      child: user != null ? BottomNavBar() : OnboardingScreen(),
+      child: (hasConnection == false)
+          ? ServerErrorScreen(
+              onPressed: () => checkInternetConnectivity(),
+            )
+          : (user != null)
+              ? BottomNavBar()
+              : OnboardingScreen(),
     );
   }
 }
